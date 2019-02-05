81e959bcbeff9682a663ef952a3b20fc5c91a2ae
Sam <Sam@mbi-macbook-17.home>
2019-02-05 23:17:05 +0000

    updated the model and added loggic to membership fee to check for minimum number

--- flatbond/api/models.py
+++ flatbond/api/models.py
@@ -3,21 +3,31 @@ from django.db import models
 class Flatbond(models.Model):
     fixed_memebership_fee = models.BooleanField(default=False)
     rent = models.IntegerField()
+    postcode = models.CharField(max_length=20, blank=True, null=True)
 
 
     @property
     def tax(self):
+        """ returns 20% Tax """
         return self.rent * 0.2
+
 
     @property
     def membership_fee(self):
         """ returns Membership fee"""
-        return self.rent + self.tax
+        if self.rent < 100:
+            return 120
+        else:
+            return self.rent + self.tax
+
 
     @property
     def fixed_memebership_fee_amount(self):
         """ returns Fixed Membership fee"""
         if self.fixed_memebership_fee == True:
-            return self.rent - self.tax
-        
+            return self.rent + self.tax
+    
+    
+    def __str__(self):
+        return self.postcode
         
