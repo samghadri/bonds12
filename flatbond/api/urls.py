5de5fb74f5490b36512a7d6fca85da833327b940
Sam <Sam@MBI-MACBOOK-17.local>
2019-02-06 08:32:02 +0000

    added detailView for flatbondapi

--- flatbond/api/urls.py
+++ flatbond/api/urls.py
@@ -2,5 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^flatbond/(?P<flatbond_id>\d+)',
+                        views.FlatBondDetailView.as_view(), name="flatbond_detail"),
+                        
     url(r'^flatbond', views.FlatBondViews.as_view(), name="flatbond_list"),
 ]
--- flatbond/api/views.py
+++ flatbond/api/views.py
@@ -3,6 +3,7 @@ from .models import Flatbond
 from .serializers import FlatBondSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
 
 
@@ -12,4 +13,18 @@ class FlatBondViews(APIView):
         serializer = FlatBondSerializer(queryset, many=True)
         result_data = serializer.data
         result = {'flatbond': result_data}
+        return Response(result)
+
+
+class FlatBondDetailView(APIView):
+
+    def get(self, request, flatbond_id=None):
+
+        try:
+            flatbond = Flatbond.objects.get(pk=flatbond_id)
+        except Flatbond.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        
+        serializer = FlatBondSerializer(flatbond)
+        result = serializer.data
         return Response(result)
